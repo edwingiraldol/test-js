77490c799cfeda50f883a00af93c5638bb1feb73
egiraldo <edwin@iMac-de-edwin.local>
2019-11-28 16:09:44 -0500

    get url

--- libraries/js/main.js
+++ libraries/js/main.js
@@ -13,22 +13,31 @@
             data: form,
             success: function(data) {
 
-                $('.result').addClass('actived')
+                $('.result').addClass('actived');
 
 
-                $('.result').append('<div class="item"> <div class="url">http://jenkins-vpc.tigocloud.net/</div> <div class="actions"> <div class="short-link">short.link/sdcsdcsd</div> <div class="primary-button copy">copy</div> </div> </div>')
+                $('.result').append('<div class="item"> <div class="url">'+url+'</div> <div class="actions"> <div class="short-link">short.link/'+data.hashid+'</div> <div class="primary-button copy">copy</div> </div> </div>');
 
+                //copiando shorted link
                 $('.result .copy').click(function(){
                     $(this).addClass('copied');
                     $(this).text('Copied !');
+                    copy_text($(this).siblings('.short-link'));
                 });
             },
             error: function() {
-            alert('There was some error performing the AJAX call!');
+            alert('no se pudo obtener la URL');
         }
         });
     });
 
 
+    function copy_text(copyText) {
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(copyText).text()).select();
+        document.execCommand("copy");
+        $temp.remove();
+    }
 
 })(jQuery);
